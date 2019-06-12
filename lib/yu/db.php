c67e874585b9e69a7e93eb36268c43fab6cc01ae
Yu Yang <yangyu823@gmail.com>
2019-06-12 10:58:28 +1000

    update db

--- lib/yu/db.php
+++ lib/yu/db.php
@@ -1,16 +1,16 @@
 <?php
 //
-//$yu_hostname = "us-cdbr-iron-east-02.cleardb.net";
-//$yu_username = "ba8e4619dcfefa";
-//$yu_password = "6d57cf09";
-//$yu_dbname = "heroku_3a0b36f24eb5945";
-//$yu_port = "3306";
+$yu_hostname = "us-cdbr-iron-east-02.cleardb.net";
+$yu_username = "ba8e4619dcfefa";
+$yu_password = "6d57cf09";
+$yu_dbname = "heroku_3a0b36f24eb5945";
+$yu_port = "3306";
 //
 //
-$yu_hostname = "127.0.0.1";
-$yu_username = "root";
-$yu_password = "root";
-$yu_dbname = "rp_db";
-$yu_port = "8889";
+//$yu_hostname = "127.0.0.1";
+//$yu_username = "root";
+//$yu_password = "root";
+//$yu_dbname = "rp_db";
+//$yu_port = "8889";
 
 ?>
